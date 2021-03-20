086cad9981f82e025fbb2eb50e171f9eb2f811b6
JJoy1219 <38539844+JJoy1219@users.noreply.github.com>
2021-03-20 19:36:31 -0400

    Add files via upload

--- lex.c
+++ lex.c
@@ -1,3 +1,5 @@
+// Authored by: Daniel Hernandez-Otero, Joel Joy
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -43,86 +45,330 @@ returnsym
 } token_type;
 
 int CheckTokenNum(char*);
+bool EndOfWord(char);
+bool IsReserved(char*);
+bool IsVar(char*);
 
 bool currentlyVar = false;
 
+char *reservedWords[] = {
+  "const",
+  "var",
+  "procedure",
+  "call",
+  "if",
+  "then",
+  "else",
+  "while",
+  "do",
+  "read",
+  "write",
+  "odd",
+  "return",
+  "begin",
+  "end"
+};
+
+char *varWords[11];
+
 int main(int argc, char *args[])
 {
+  // initialize varWords
+  for (int i = 0; i < 15; i++)
+  {
+    varWords[i] = malloc(11 * sizeof(char));
+  }
+
+
   char character;
-  int stringlen = 0;
-  char *tokenList = "";
+  char futureChar;
+
+  char *tokenList = malloc(200 * sizeof(char));
+  char *conversion = malloc(sizeof(char));
+  char word[50];
+
+  int wordCounter = 0, varCount = 0;
+
+  bool isComment = false;
 
   FILE *input = fopen(args[1], "r");
+
+  memset(word, 0, sizeof(word));
 
   printf("\n\nLexeme Table:\n\tLexeme\t\ttoken type\n");
 
   while (!feof(input))
   {
-    character = fgetc(input);
-    if(character == ' ')
-      continue;
+    int token = 0;
 
-    int stringLength = (int)strlen(word), token = 0;
+    fscanf(input, "%c", &character);
 
-
-    // ERROR HANDLING HERE
-    
-
-
-    // end error handling
-    if(isalpha(character))
+    if((iscntrl(character) || EndOfWord(character)) && wordCounter > 0)
     {
-      stringlen++;
-      if (stringlen > 11)
-        printf("Error : Identifier names cannot exceed 11 characters");
-      //process string 
+      wordCounter = 0;
+
+      if (!isComment)
+      {
+        // This is a reserved word
+        if (IsReserved(word))
+        {
+          //printf("Reserved word: %s\n", word);
+        }
+        else if (!isdigit(character))
+        {
+          // This is a reserved variable already
+          if (varCount > 0 && IsVar(word))
+          {
+            //printf("Reserved var: %s\n", word);
+          }
+          else if (!(strcmp(word, " ") == 0))
+          {
+            // This is a new variable
+            strcpy(varWords[varCount], word);
+            varCount++;
+            if (strlen(word) > 11)
+               printf("Error : Identifier names cannot exceed 11 characters\n");
+          }
+        }
+
+        if (isdigit(character))
+          printf("Number\n");
+
+        // Here comes the big ladder
+        token = CheckTokenNum(word);
+        if (!(strlen(word) > 11))
+        {
+          printf("\t%s\t\t%d\n", word, token);
+          sprintf(conversion,"%d ", token);
+          strncat(tokenList, conversion, strlen(conversion));
+          printf("Adding %s to tokenList\n", conversion);
+        }
+        if (token == 2)
+        {
+          strncat(tokenList, word, strlen(word));
+          printf("Here\n");
+          strncat(tokenList, " ", 1);
+        }
+
+
+        if (strcmp(word, "end") == 0)
+        {
+          char lastChar[2];
+
+          memset(lastChar, 0, sizeof(lastChar));
+
+          lastChar[0] = character;
+
+          token = CheckTokenNum(lastChar);
+          printf("\t%c\t\t%d\n", character, token);
+          sprintf(conversion,"%d ", token);
+          strncat(tokenList, conversion, strlen(conversion));
+          break;
+        }
+
+        // Reset word
+        memset(word, 0, sizeof(word));
+
+        // Recognize special character
+        if (EndOfWord(character) && character != ' ')
+        {
+          char special[2];
+
+          memset(special, 0, sizeof(special));
+
+          special[0] = character;
+
+          token = CheckTokenNum(special);
+
+          printf("\t%s\t\t%d\n", special, token);
+          sprintf(conversion,"%d ", token);
+          strncat(tokenList, conversion, strlen(conversion));
+        }
+
+      }
     }
-    else if(isdigit(character))
+    else if (wordCounter == 0 && EndOfWord(character) && character != ' ')
     {
-      //process number
-    }
-    else if(isctrl(character))
-    {
-      continue; 
+      // Handle special, SPECIAL characters here
+      // like /*, */, >=, := etc.
+
+      char recent[5];
+
+      memset(recent, 0, sizeof(recent));
+
+      if (!isComment)
+      {
+        if (character == '/')
+        {
+          fscanf(input, "%c", &futureChar);
+          if (futureChar == '*')
+            isComment = true;
+          else
+            fseek(input, -1, SEEK_CUR);
+        }
+        else if (character == '*')
+        {
+          fscanf(input, "%c", &futureChar);
+          if (futureChar == '/')
+            isComment = false;
+          else
+          {
+            recent[0] = character;
+            token = CheckTokenNum(recent);
+            printf("\t%c\t\t%d\n", character, token);
+            sprintf(conversion,"%d ", token);
+            strncat(tokenList, conversion, strlen(conversion));
+            fseek(input, -1, SEEK_CUR);
+          }
+        }
+        else if (character == ':')
+        {
+          fscanf(input, "%c", &futureChar);
+          if (futureChar == '=')
+          {
+            recent[0] = character;
+            recent[1] = futureChar;
+
+            token = CheckTokenNum(recent);
+
+            printf("\t%s\t\t%d\n", recent, token);
+            sprintf(conversion,"%d ", token);
+            strncat(tokenList, conversion, strlen(conversion));
+          }
+          else
+            fseek(input, -1, SEEK_CUR);
+        }
+        else if (character == '<')
+        {
+          fscanf(input, "%c", &futureChar);
+          if (futureChar == '>' || futureChar == '=')
+          {
+            recent[0] = character;
+            recent[1] = futureChar;
+
+            token = CheckTokenNum(recent);
+
+            printf("\t%s\t\t%d\n", recent, token);
+            sprintf(conversion,"%d ", token);
+            strncat(tokenList, conversion, strlen(conversion));
+          }
+          else
+            fseek(input, -1, SEEK_CUR);
+        }
+        else if (character == '>')
+        {
+          fscanf(input, "%c", &futureChar);
+          if (futureChar == '=')
+          {
+            recent[0] = character;
+            recent[1] = futureChar;
+
+            token = CheckTokenNum(recent);
+
+            printf("\t%s\t\t%d\n", recent, token);
+            sprintf(conversion,"%d ", token);
+            strncat(tokenList, conversion, strlen(conversion));
+          }
+          else
+            fseek(input, -1, SEEK_CUR);
+        }
+        else
+        {
+          recent[0] = character;
+
+          token = CheckTokenNum(recent);
+
+          printf("\t%s\t\t%d\n", recent, token);
+          sprintf(conversion,"%d ", token);
+          strncat(tokenList, conversion, strlen(conversion));
+        }
+      }
+
     }
     else
     {
-      token = CheckTokenNum(character);  
-    }
-    
-    // Push the pointer back by one if "var" name ends with , or ; or something similar
-    if (stringLength >= 2)
-    {
-      switch (word[stringLength - 1])
+      // Add to the word here
+      if (isalpha(character))
       {
-        case ',':
-        case ';':
-        case '.':
-          word[stringLength - 1] = 0;
-          fseek(input, -1, SEEK_CUR);
-          stringLength = (int)strlen(word);
-          break;
-        default:
-          break;
+        word[wordCounter] = character;
+        wordCounter++;
+
+      }
+      else if (!iscntrl(character) && character != ' ')
+      {
+        printf("\t%c\t\t3\n", character);
+        sprintf(conversion,"%d ", 3);
+        strncat(tokenList, conversion, strlen(conversion));
+        strncat(tokenList, &character, 1);
+        strncat(tokenList, " ", 1);
       }
     }
 
-    // Here comes the big ladder
-    token = CheckTokenNum(word);
+    // ERROR HANDLING HERE
 
-
-    printf("\t%s\t\t%d\n", word, token);
   }
-
+  printf("%s",tokenList);
   fclose(input);
 
   return 0;
 }
 
-int CheckTokenNum(char symbol)
+bool EndOfWord(char character)
+{
+  char specialCharacters[] = {
+    ' ',
+    '+',
+    '-',
+    '*',
+    '/',
+    '(',
+    ')',
+    '=',
+    ',',
+    '.',
+    '<',
+    '>',
+    ';',
+    ':',
+    '%'
+  };
+
+  for (int i = 0; i < strlen(specialCharacters); i++)
+  {
+    if (character == specialCharacters[i])
+      return true;
+  }
+
+  return false;
+}
+
+bool IsReserved(char *word)
+{
+  for (int i = 0; i < 15; i++)
+  {
+    if (strcmp(word, reservedWords[i]) == 0)
+      return true;
+  }
+
+  return false;
+}
+
+bool IsVar(char *word)
+{
+  for (int i = 0; i < 15; i++)
+  {
+    //printf("\t\t%d. %s\n", i+1, varWords[i]);
+    if (strcmp(word, varWords[i]) == 0)
+      return true;
+  }
+
+  return false;
+}
+
+int CheckTokenNum(char *word)
 {
   // this is really ugly I'm not a fan of this but idk what else to do don't sue me
-  if (symbol == '%')
+  if (strcmp(word, "%") == 0)
     return 1;
   else if (strcmp(word, "+") == 0)
     return 4;
@@ -184,7 +430,7 @@ int CheckTokenNum(char symbol)
   }
   else
   {
-    if (currentlyVar)
+    if (currentlyVar || IsVar(word))
       return 2;
 
     if (isdigit(word[(int)strlen(word) - 1]))
